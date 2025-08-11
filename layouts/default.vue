62a850571d9e325fd2c276b4b6517bffd6a9fff4
Mitchell <m_parsons94@hotmail.com>
2025-08-11 18:50:48 +1000

    layout test two

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,13 @@
 
 <template>
   <div>
-    <Header />
+    <GlobalHeader />
       <slot></slot>
-    <Footer />
+    <GlobalFooter />
   </div>
 </template>
+
+<script setup>
+import GlobalHeader from '@/components/global/Header.vue'
+import GlobalFooter from '@/components/global/Footer.vue'
+</script>
